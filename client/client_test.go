88639edf682049287cc03422b16e3f20c0687028
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-09-19 19:54:12 +0300

    feat: implement various test cases for GetDateTime function

--- client/client_test.go
+++ client/client_test.go
@@ -1,20 +1,22 @@
-package main
+package client
 
 import (
+	"errors"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 )
 
 func TestSetupEnvironment(t *testing.T) {
-	// Test values
 	testURL := "http://testserver"
 	testPort := "8080"
 	testPortGin := "9090"
 
-	// Call the function to set up the environment
 	SetupEnv(testURL, testPort, testPortGin)
 
-	// Check if the environment variables are set correctly
 	if os.Getenv("SERVER_URL") != testURL {
 		t.Errorf("SERVER_URL not set correctly. Expected %s, got %s", testURL, os.Getenv("SERVER_URL"))
 	}
@@ -26,4 +28,86 @@ func TestSetupEnvironment(t *testing.T) {
 	if os.Getenv("SERVER_PORT_GIN") != testPortGin {
 		t.Errorf("SERVER_PORT_GIN not set correctly. Expected %s, got %s", testPortGin, os.Getenv("SERVER_PORT_GIN"))
 	}
+}
+
+func mockServer(t *testing.T, contentType string, responseBody string, statusCode int) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", contentType)
+		w.WriteHeader(statusCode)
+		io.WriteString(w, responseBody)
+	}))
+}
+
+func TestGetDateTime(t *testing.T) {
+	serverPlain := mockServer(t, "text/plain", "2024-09-19T12:34:56Z", http.StatusOK)
+	defer serverPlain.Close()
+
+	serverJSON := mockServer(t, "application/json", `{"datetime": "2024-09-19T12:34:56Z"}`, http.StatusOK)
+	defer serverJSON.Close()
+
+	serverFail := mockServer(t, "application/json", ``, http.StatusInternalServerError)
+	defer serverFail.Close()
+
+	serverURLPlain := serverPlain.URL[:strings.LastIndex(serverPlain.URL, ":")]
+	serverPortPlain := serverPlain.URL[strings.LastIndex(serverPlain.URL, ":")+1:]
+
+	serverURLJSON := serverJSON.URL[:strings.LastIndex(serverJSON.URL, ":")]
+	serverPortJSON := serverJSON.URL[strings.LastIndex(serverJSON.URL, ":")+1:]
+
+	t.Run("Success - Plain text response", func(t *testing.T) {
+		SetupEnv(serverURLPlain, serverPortPlain, serverPortPlain)
+		dateTime, err := GetDateTime("other", "text/plain")
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+		if dateTime != "2024-09-19T12:34:56Z" {
+			t.Errorf("Expected datetime to be '2024-09-19T12:34:56Z', got %s", dateTime)
+		}
+	})
+
+	t.Run("Success - JSON response", func(t *testing.T) {
+		SetupEnv(serverURLJSON, serverPortJSON, serverPortJSON)
+		dateTime, err := GetDateTime("gin", "application/json")
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+		if dateTime != "2024-09-19T12:34:56Z" {
+			t.Errorf("Expected datetime to be '2024-09-19T12:34:56Z', got %s", dateTime)
+		}
+	})
+
+	t.Run("Error - Missing SERVER_URL", func(t *testing.T) {
+		os.Setenv("SERVER_URL", "")
+		_, err := GetDateTime("gin", "application/json")
+		if err == nil {
+			t.Fatal("Expected error for missing SERVER_URL, got none")
+		}
+		os.Setenv("SERVER_URL", serverURLJSON)
+	})
+
+	t.Run("Error - Invalid Content-Type", func(t *testing.T) {
+		SetupEnv(serverURLPlain, serverPortPlain, serverPortPlain)
+		_, err := GetDateTime("other", "unsupported/type")
+		if err == nil {
+			t.Fatal("Expected error for unsupported content type, got none")
+		}
+	})
+
+	t.Run("Error - HTTP request failure", func(t *testing.T) {
+		SetupEnv("", "invalidPort", "invalidPort")
+		_, err := GetDateTime("other", "text/plain")
+		if !errors.Is(err, ErrURLandPortMustBeSet) {
+			t.Fatalf("Expected ErrURLandPortMustBeSet, got %v", err)
+		}
+	})
+
+	t.Run("Error - Failed server response", func(t *testing.T) {
+		serverURLFail := serverFail.URL[:strings.LastIndex(serverFail.URL, ":")]
+		serverPortFail := serverFail.URL[strings.LastIndex(serverFail.URL, ":")+1:]
+		SetupEnv(serverURLFail, serverPortFail, serverPortFail)
+		_, err := GetDateTime("gin", "application/json")
+		if err == nil {
+			t.Fatal("Expected error for failed server response, got none")
+		}
+	})
 }
